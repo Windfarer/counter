6b062cff15b3bf35acae575fee879f8fe7f7bf4a
Qizhao Yang <windfarer@gmail.com>
2016-03-29 22:27:26 +0800

    add button

--- counter.py
+++ counter.py
@@ -1,9 +1,9 @@
-from flask import Flask, jsonify, current_app
+from flask import Flask, jsonify, current_app, send_file, request
 import redis
 import os
 
-REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR')
-REDIS_PORT = os.environ.get('REDIS_PORT_6379_TCP_PORT')
+REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR') or 'localhost'
+REDIS_PORT = os.environ.get('REDIS_PORT_6379_TCP_PORT') or 6379
 REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
 app = Flask(__name__)
 app.redis = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
@@ -15,11 +15,16 @@ def init_redis():
         current_app.redis.set('counter', 0)
 
 
-@app.route('/counter')
-def counter():
-    current_app.redis.incr('counter')
-    return jsonify({"counter": int(current_app.redis.get('counter'))})
+@app.route('/')
+def index():
+    return send_file('static/index.html')
 
+
+@app.route('/counter', methods=["GET", "PUT"])
+def get_counter():
+    if request.method == 'PUT':
+        current_app.redis.incr('counter')
+    return jsonify({"counter": int(current_app.redis.get('counter'))})
 
 if __name__ == '__main__':
     app.run(debug=True)
