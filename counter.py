d89c06ed7d16400a5b92e8179dba7d7862aa0b7f
Windfarer <windfarer@gmail.com>
2017-08-24 16:28:51 +0800

    update, add local counter

--- counter.py
+++ counter.py
@@ -2,29 +2,50 @@ from flask import Flask, jsonify, current_app, send_file, request, render_templa
 import redis
 import os
 
+REDIS_ENABLED = os.environ.get('REDIS_ENABLED', "false").lower() == 'true'
 REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR') or 'localhost'
 REDIS_PORT = os.environ.get('REDIS_PORT_6379_TCP_PORT') or 6379
 REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
 app = Flask(__name__)
-app.redis = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
 
+class Counter(object):
+    _redis = None
 
-@app.before_first_request
-def init_redis():
-    if not current_app.redis.get('counter'):
-        current_app.redis.set('counter', 0)
+    def __init__(self, redis=None):
+        if redis:
+            self._redis = redis
+            if not self._redis.get('counter'):
+                self._redis.set('counter', 0)
+        else:
+            self._local_counter = 0
 
+    def incr(self):
+        if self._redis:
+            self._redis.incr('counter')
+        else:
+            self._local_counter += 1
+
+    def get(self):
+        if self._redis:
+            return self._redis.get('counter')
+        else:
+            return self._local_counter
+
+if REDIS_ENABLED:
+    app.counter = Counter(redis=redis.Redis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD))
+else:
+    app.counter = Counter()
 
 @app.route('/')
 def index():
-    return render_template('index.html', counter=int(current_app.redis.get('counter')))
+    return render_template('index.html', counter=int(current_app.counter.get()))
 
 
 @app.route('/counter', methods=["GET", "PUT"])
 def get_counter():
     if request.method == 'PUT':
-        current_app.redis.incr('counter')
-    return jsonify({"counter": int(current_app.redis.get('counter'))})
+        current_app.counter.incr()
+    return jsonify({"counter": int(current_app.counter.get())})
 
 if __name__ == '__main__':
     app.run(debug=True)
